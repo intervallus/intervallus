c27ae25b09e862b9cba227d1f3e56c3788464510
Luana Ferreira <luanaferreirasilva2006@gmailcom>
2026-05-27 01:01:49 -0300

    feat: add POST route and resolve require issues

--- backend/intervallus/src/intervallus/core.clj
+++ backend/intervallus/src/intervallus/core.clj
@@ -1,12 +1,16 @@
 (ns intervallus.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
-            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [ring.middleware.params :refer [wrap-params]]
             [intervallus.rotas :refer [app-routes]])
   (:gen-class))
 
-(def app (-> app-routes wrap-params wrap-json-response))
+(def app
+ (-> app-routes
+      wrap-params
+      (wrap-json-body {:keywords? false})
+      wrap-json-response))
 
 (defn -main []
-  (println "Servidor na porta 3000")
+  (println "Servidor na porta 3000")  
   (run-jetty app {:port 3000 :join? true}))
--- backend/intervallus/src/intervallus/rotas.clj
+++ backend/intervallus/src/intervallus/rotas.clj
@@ -1,13 +1,19 @@
 (ns intervallus.rotas
-  (:require [compojure.core :refer [defroutes GET]]
+  (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :as route]
             [intervallus.calculos :as calc]))
 
+(defn processar [escala trastes]
+  (let [L (try (Double/parseDouble (str escala)) (catch Exception _ nil))
+        n (or (try (Integer/parseInt (str trastes)) (catch Exception _ nil)) 22)]
+    (if (and L (>= L 400) (<= L 1000))
+      {:status 200 :body {:escala-mm L :trastes (calc/calcular L n)}}
+      {:status 400 :body {:erro "Informe 'escala' entre 400 e 1000 mm"}})))
+
 (defroutes app-routes
   (GET "/api/trastes" [escala trastes]
-    (let [L (try (Double/parseDouble escala) (catch Exception _ nil))
-          n (or (try (Integer/parseInt trastes) (catch Exception _ nil)) 22)]
-      (if (and L (>= L 400) (<= L 1000))
-        {:status 200 :body {:escala-mm L :trastes (calc/calcular L n)}}
-        {:status 400 :body {:erro "Informe 'escala' entre 400 e 1000 mm"}})))
-  (route/not-found {:status 404 :body {:erro "Rota não encontrada"}}))  
+    (processar escala trastes))
+  (POST "/api/trastes" req
+    (let [{:strs [escala trastes]} (:body req)]
+      (processar escala trastes)))
+  (route/not-found {:status 404 :body {:erro "Rota não encontrada"}}))
